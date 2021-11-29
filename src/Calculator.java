e492e0b2012b906e090f8ec0e048be96b84bd3be
RP-content <info@rp-content.de>
2021-11-29 21:25:20 +0100

    Multithreading test 1.0

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,32 @@
-public class Calculator implements Runnable{
-    Calculator(float X,float Y,int iterations){
-
-
+public class Calculator extends Thread{
+    private double X,Y;
+    private int iterations,px,py;
+    private Main m;
+    Calculator(double X,double Y,int px,int py,int iterations,Main m){
+        this.iterations=iterations;
+        this.X=X;
+        this.Y=Y;
+        this.m=m;
+        this.px=px;
+        this.py=py;
     }
 
     @Override
     public void run() {
+        double d =mandelbrot(new Complex(X,Y));
+        m.cache[px][py]=d;
 
     }
 
+    public double mandelbrot(Complex c){
+        Complex z =new Complex(0,0);
+        for (int i=0;i<iterations;i++){
+            z=z.add(z.multiply(z,z),c);
+            if(4<z.getLength(z)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
